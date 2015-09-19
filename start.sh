448dc47aeaa3c3994099011d16e0b474752ad39c
Antonio J F Sousa <antonio@tradingeconomics.com>
2015-09-19 19:07:30 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,5 @@
 echo 'Starting HBASE'
 /opt/hbase/bin/start-hbase.sh
 
-echo 'Waiting a bit for HBASE before creating TSDB Tables'
-sleep 10
-
-echo 'Begun creating TSDB tables'
-export HBASE_HOME=/opt/hbase
-export COMPRESSION=NONE
-curl -s https://raw.githubusercontent.com/OpenTSDB/opentsdb/master/src/create_table.sh | bash
-
 echo 'Leave a loop scrip running'
 /bin/sh -c "while true; do echo 'HBASE RUNNING'; /bin/date; sleep 1; done" 

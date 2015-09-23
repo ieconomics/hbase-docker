19a572fe7f96b8f3b3bed03fd29e0b6f5259d4ce
EC2 Default User <ec2-user@ip-10-0-1-25.ec2.internal>
2015-09-23 14:39:31 +0000

    -

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+echo 'Starting HBASE'
+/opt/hbase/bin/start-hbase.sh
+
+#echo 'Leave a loop scrip running'
+#/bin/sh -c "while true; do echo 'HBASE RUNNING'; /bin/date; sleep 1; done" 

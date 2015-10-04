5034da16518aa17cbf4651f139a51529bfe4562f
EC2 Default User <ec2-user@ip-10-0-1-25.ec2.internal>
2015-10-04 02:08:17 +0000

    -

--- distributed/entrypoint.sh
+++ distributed/entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+
+
+echo 'RUNNING ENTRYPOINT'
+
 ZK=${ZK:localhost}
 echo 'SETTING ZOOKEEPER TO'
 echo $ZK
 sed "s/ZK/$ZK/" hbase-site-template.xml > /opt/hbase/conf/hbase-site.xml
 cat /opt/hbase/conf/hbase-site.xml
+
+set -e
+
+exec "$@"
